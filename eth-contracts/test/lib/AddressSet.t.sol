f9fbd61f9cb88c26cadc6f8a4e422c73e6e560ea
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-02-19 21:50:33 +0530

    initial tests

--- eth-contracts/test/lib/AddressSet.t.sol
+++ eth-contracts/test/lib/AddressSet.t.sol
@@ -2,15 +2,27 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {AddressSet} from "../src/lib/AddressSet.sol";
+import {AddressSet} from "../../src/lib/AddressSet.sol";
 
-contract AddressSet is Test {
+contract AddressSetTest is Test {
     using AddressSet for AddressSet.Set;
 
-    AddressSet.Set public set;
+    AddressSet.Set tokenSet;
 
     function setUp() public {
-        set = AddressSet.Set();
+    }
+
+    function test_insert(address randomAddress) public {
+        tokenSet.insert(randomAddress);
+        assert(tokenSet.contains(randomAddress));
+    }
+
+    function test_remove(address randomAddress1, address randomAddress2) public {
+        tokenSet.insert(randomAddress1);
+        tokenSet.insert(randomAddress2);
+        tokenSet.remove(randomAddress1);
+        assert(!tokenSet.contains(randomAddress1));
+        assert(tokenSet.contains(randomAddress2));
     }
 
 //    function test_Increment() public {
